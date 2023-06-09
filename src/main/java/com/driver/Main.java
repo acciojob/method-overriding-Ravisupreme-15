c7c144936427d6a1d2b9a8a32aa5940d22d9aa7e
RaviSupreme-15 <supremeravi1819@gmail.com>
2023-06-09 17:14:40 +0530

    commit changes observed overriging

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+
+
+
+    public static void main(String[] args) {
+
+          B b = new B();
+          System.out.println(b.meth());
+
+          B a = new B();
+
+          System.out.println(a.meth());
+
+
+    }
   
 }
